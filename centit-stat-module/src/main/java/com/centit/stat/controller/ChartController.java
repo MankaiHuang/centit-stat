8614b78684925f3a5da7b31f32d5b073a7282e38
zhf <zhf@centit.com>
2019-08-21 16:59:19 +0800

    增加应用管理查询条件

--- centit-stat-module/src/main/java/com/centit/stat/controller/ChartController.java
+++ centit-stat-module/src/main/java/com/centit/stat/controller/ChartController.java
@@ -19,7 +19,7 @@ import fr.opensagres.xdocreport.template.TemplateEngineKind;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
