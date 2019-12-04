8150d90f6ae0f70b29dc461834398980c87fac29
zhf <zhf@centit.com>
2019-12-04 14:48:16 +0800

    report修改word上传

--- centit-stat-web/src/main/java/com/centit/stat/config/NormalSpringMvcConfig.java
+++ centit-stat-web/src/main/java/com/centit/stat/config/NormalSpringMvcConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Import;
 /**
  * Created by zou_wy on 2017/3/29.
  */
-@Import(WebConfig.class)
 @ComponentScan(basePackages = {"com.centit.stat.controller"},
         includeFilters = {@ComponentScan.Filter(value= org.springframework.stereotype.Controller.class)},
         useDefaultFilters = false)
